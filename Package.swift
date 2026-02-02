28d2f02c70bf13e63109fb198ee4bb66bd4fa978
pangu25 <admin@enteleca.com>
2026-02-02 12:59:18 -0600

    feat: Ventura (macOS 13.x) compatibility
    - Changed swift-tools-version from 6.0 to 5.9
    - Changed platform target from macOS 14 to macOS 13
    - Updated Commander dependency to pangu25/Commander (Ventura-compatible fork)
    - Updated README to reflect macOS 13+ requirement
    
    This allows imsg to run on older Macs that cannot upgrade to Sonoma.

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "imsg",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(name: "IMsgCore", targets: ["IMsgCore"]),
         .executable(name: "imsg", targets: ["imsg"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/steipete/Commander.git", from: "0.2.0"),
+        .package(url: "https://github.com/pangu25/Commander.git", branch: "main"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.4"),
         .package(url: "https://github.com/marmelroy/PhoneNumberKit.git", from: "4.2.2"),
     ],
